fbcc4a792be5f2b52898ec8956183b10724bb343
tungpt <ht@thanhtunguet.info>
2020-04-26 22:42:14 +0700

    Add README and Singleton

---
+++ README.md
@@ -0,0 +1,6 @@
+react3l-core
+============
+
+The source of this package has been moved to [react3l](https://github.com/thanhtunguet/react3l) repo.
+
+&copy;2020 thanhtunguet
--- src/repositories/Repository.ts
+++ src/repositories/Repository.ts
@@ -1,16 +1,12 @@
-import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
-import { Axios } from 'axios-observable';
+import axios, {AxiosError, AxiosInstance, AxiosRequestConfig, AxiosResponse} from 'axios';
+import {Axios} from 'axios-observable';
 
 export class Repository {
-  protected http: AxiosInstance;
-
-  protected httpObservable: Axios;
-
   public static requestInterceptor: (config: AxiosRequestConfig) => AxiosRequestConfig | Promise<AxiosRequestConfig>;
-
   public static responseInterceptor: (response: AxiosResponse) => AxiosResponse | Promise<AxiosResponse>;
-
   public static errorInterceptor: (error: AxiosError) => AxiosError | Promise<AxiosError>;
+  protected http: AxiosInstance;
+  protected httpObservable: Axios;
 
   public constructor(httpConfig: AxiosRequestConfig) {
     this.http = axios.create(httpConfig);
@@ -27,8 +23,16 @@ export class Repository {
     }
   }
 
-  public setBaseURL(baseURL: string) {
+  public get baseURL() {
+    return this.http.defaults.baseURL ?? this.httpObservable.defaults.baseURL ?? null;
+  }
+
+  public set baseURL(baseURL: string) {
     this.http.defaults.baseURL = baseURL;
     this.httpObservable.defaults.baseURL = baseURL;
+  }
+
+  public setBaseURL(baseURL: string) {
+    this.baseURL = baseURL;
   }
 }
